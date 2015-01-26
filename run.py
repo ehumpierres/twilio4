f2627512072117cdf3701951e58fd2e435d2e7e3
ehumpierres <ehumpierres@Ernestos-MacBook-Air-2.local>
2015-01-26 18:39:26 -0500

    test

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, session
 import twilio.twiml
 
 # The session object makes use of a secret key.
-SECRET_KEY = 'a secret key'
+SECRET_KEY = 'd634dffc544604661e8749d6328c413c'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
@@ -15,29 +15,15 @@ callers = {
 }
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
+def getConvo():
     """Respond with the number of text messages sent between two parties."""
  
-    counter = session.get('counter', 0)
+ 	username = request.cookies.get('ernesto@gotrotter.com')
+
  
-    # increment the counter
-    counter += 1
- 
-    # Save the new counter value in the session
-    session['counter'] = counter
- 
-    from_number = request.values.get('From')
-    if from_number in callers:
-        name = callers[from_number]
-    else:
-        name = "Monkey"
- 
-    message = "".join([name, " has messaged ", request.values.get('To'), " ", 
-        str(counter), " times."])
-    resp = twilio.twiml.Response()
-    resp.sms(message)
- 
-    return str(resp)
+  	return username
+
+
  
 if __name__ == "__main__":
     app.run(debug=True)
