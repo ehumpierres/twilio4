2a187475e58d5dd31fc68f06d32c35ea91060a2f
ehumpierres <ehumpierres@Ernestos-MacBook-Air-2.local>
2015-01-26 20:03:16 -0500

    test7

--- run.py
+++ run.py
@@ -1,15 +1,29 @@
 from flask import Flask, request, redirect
 import twilio.twiml
  
+SECRET_KEY = 'd634dffc544604661e8749d6328c413c'
 app = Flask(__name__)
+app.config.from_object(__name__)
  
+
 @app.route("/", methods=['GET', 'POST'])
 def getCookie():
     """Respond to incoming calls with a simple text message."""
  
+    counter = session.get('counter', 0)
+ 
+    # increment the counter
+    counter += 1
+ 
+    # Save the new counter value in the session
+    session['counter'] = counter
+
     resp = twilio.twiml.Response()
     resp.message("Hello, Mobile Monkey")
     return str(resp)
+
+
+
  
 if __name__ == "__main__":
     app.run(debug=True)
