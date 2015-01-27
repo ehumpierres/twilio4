8def4c228e625d77d0213cd8fc7b79ff496d46ac
ehumpierres <ehumpierres@Ernestos-MacBook-Air-2.local>
2015-01-27 00:03:03 -0500

    test17

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, session
 from datetime import datetime, timedelta
 from twilio import twiml
+from twilio.rest import TwilioRestClient
  
 app = Flask(__name__)
  
@@ -8,22 +9,22 @@ app = Flask(__name__)
 def sms():
  
     #get the cookie value, or default to zero
-    messagecount = int(request.cookies.get('messagecount',0))
-    messagecount += 1
+    #messagecount = int(request.cookies.get('messagecount',0))
+    #messagecount += 1
  
     twml = twiml.Response()
 
-    body = request.form['Body']
+    #body = request.form['Body']
     #twml.sms("You've sent " + str(messagecount) + " messages in this conversation so far")
  
-    resp = make_response(str(body))
+    #resp = make_response(str(body))
  
     #expires=datetime.utcnow() + timedelta(hours=4)
     #resp.set_cookie('messagecount',value=str(messagecount),expires=expires.strftime('%a, %d %b %Y %H:%M:%S GMT'))
 
 
  
-    return resp
+    return str(twml)
  
 if __name__ == "__main__":
     app.debug = True
